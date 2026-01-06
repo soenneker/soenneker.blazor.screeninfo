a8405e652aa49148d57d13903f2449ae44d3ea63
Jake Soenneker <jake@soenneker.com>
2026-01-06 10:51:12 -0600

    Reduces allocation

--- src/ScreenInfoInterop.cs
+++ src/ScreenInfoInterop.cs
@@ -1,11 +1,9 @@
-using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.JSInterop;
 using Soenneker.Blazor.ScreenInfo.Abstract;
 using Soenneker.Blazor.ScreenInfo.Dtos;
 using Soenneker.Blazor.Utils.ResourceLoader.Abstract;
-using Soenneker.Extensions.ValueTask;
 using Soenneker.Asyncs.Initializers;
 
 namespace Soenneker.Blazor.ScreenInfo;
@@ -25,23 +23,24 @@ public sealed class ScreenInfoInterop : IScreenInfoInterop
     {
         _jsRuntime = jSRuntime;
         _resourceLoader = resourceLoader;
-
-        _scriptInitializer = new AsyncInitializer(async token =>
-        {
-            await _resourceLoader.ImportModuleAndWaitUntilAvailable(_modulePath, _moduleNamespace, 100, token);
-        });
+        _scriptInitializer = new AsyncInitializer(InitializeScript);
     }
 
-    public async ValueTask Warmup(CancellationToken cancellationToken = default)
+    private ValueTask InitializeScript(CancellationToken token)
     {
-        await _scriptInitializer.Init(cancellationToken);
+        return _resourceLoader.ImportModuleAndWaitUntilAvailable(_modulePath, _moduleNamespace, 100, token);
+    }
+
+    public ValueTask Warmup(CancellationToken cancellationToken = default)
+    {
+        return _scriptInitializer.Init(cancellationToken);
     }
 
     public async ValueTask<ScreenInfoDto> Get(CancellationToken cancellationToken = default)
     {
         await _scriptInitializer.Init(cancellationToken);
 
-        return await _jsRuntime.InvokeAsync<ScreenInfoDto>($"{_moduleNamespace}.get", cancellationToken);
+        return await _jsRuntime.InvokeAsync<ScreenInfoDto>("ScreenInfoInterop.get", cancellationToken);
     }
 
     public async ValueTask DisposeAsync()
